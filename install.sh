4476618d974cb09f39f064ce756eff7aba0425a3
rick <rick@ricks-MacBook-Air.local>
2020-02-20 21:57:45 -0500

    add notify repo

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ cd $( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 [[ -f submodules/iterm-notify/notify.py ]] || echo Missing iterm-notify
 [[ -d ~/Library/ApplicationSupport/iTerm2/Scripts/AutoLaunch ]] || mkdir -p ~/Library/ApplicationSupport/iTerm2/Scripts/AutoLaunch
 [[ -f ~/Library/ApplicationSupport/iTerm2/Scripts/AutoLaunch/iterm-notify.py ]] || cp submodules/iterm-notify/notify.py ~/Library/ApplicationSupport/iTerm2/Scripts/AutoLaunch/iterm-notify.py
+[[ -f ~/.sendfile.sh ]] || cp sendfile.sh ~/.sendfile.sh
 [[ -f ~/.iterm-notify-init.sh ]] || cp submodules/iterm-notify/init.sh ~/.iterm-notify-init.sh
 [[ -f ~/.iterm-notify-identity ]] || openssl rand -base64 32|head -n1 > ~/.iterm-notify-identity && chmod 0600 ~/.iterm-notify-identity
 
 
 grep -q iterm-notify-init ~/.profile.sh || echo 'source ~/.iterm-notify-init.sh' >> ~/.profile.sh
+grep -q sendfile ~/.profile.sh || echo 'source ~/.sendfile.sh' >> ~/.profile.sh
 
 
